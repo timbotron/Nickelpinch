f48a3811a670cab89e4c18949ac9c6d55800d6c3
Tim Habersack <tim.habersack@gmail.com>
2014-03-12 00:47:19 -0500

    have new account creation form + logic in place.

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -12,7 +12,41 @@ class HomeController extends BaseController {
 	
 	public function register()
 	{
+		View::share('default_currency',Config::get('nickelpinch.default_currency'));
 		return View::make('home.account_create');
+	}
+
+	public function process_register()
+	{
+		$rules = [
+					'username' 		=> 'required|alpha_dash|unique:users,username|between:4,30',
+					'password'		=> 'required|alpha_dash|between:4,60',
+					'email'			=> 'required|email|unique:users,email|max:90',
+					'currency'		=> 'required',
+		];
+
+		$validator = Validator::make(Input::all(), $rules);
+
+		if($validator->passes())
+		{
+			// Gotta store user
+			$user = new User;
+			$user->username = Input::get('username');
+			$user->email = Input::get('email');
+			$user->currency = Input::get('currency');
+			$user->rank = Config::get('nickelpinch.default_rank');
+			$user->password = Hash::make(Input::get('password'));
+			$user->save();
+
+			// Hash::make($password);
+			return Redirect::to('/');
+		}
+		else
+		{
+			return Redirect::to('/register')->withErrors($validator)->withInput();
+		}
+
+
 	}
 
 }
--- app/views/home/account_create.blade.php
+++ app/views/home/account_create.blade.php
@@ -7,18 +7,26 @@
 			{{ Form::open(array('url' => 'register','class'=>'form')) }}
 			<fieldset>
 			<legend>Create an Account</legend>
+			@foreach($errors->all() as $message)
+			        <div class="alert alert-warning">{{ $message }}</div>
+			@endforeach
 
 				<div class="form-group">
 					{{ Form::label('email', 'E-Mail Address'); }}
-					{{ Form::email('email',null,['class'=>'form-control','placeholder'=>'Enter email']) }}
+					{{ Form::email('email',null,array('class'=>'form-control','placeholder'=>'Enter email')) }}
 				</div>
 				<div class="form-group">
 					{{ Form::label('username', 'Username'); }}
-					{{ Form::text('username',null,['class'=>'form-control','placeholder'=>'Username']) }}
+					{{ Form::text('username',null,array('class'=>'form-control','placeholder'=>'Username')) }}
 				</div>
 				<div class="form-group">
 					{{ Form::label('password', 'Password'); }}
-					{{ Form::password('password',['class'=>'form-control','placeholder'=>'Password']) }}
+					{{ Form::password('password',array('class'=>'form-control','placeholder'=>'Password')) }}
+				</div>
+				<div class="form-group">
+					{{ Form::label('currency', 'Choose your Currency'); }}
+					{{ Form::select('currency',$currencies,$default_currency,array('class'=>'form-control')) }}
+					<span class="help-block">Just to know which symbol to display.</span>
 				</div>
 
 			   
