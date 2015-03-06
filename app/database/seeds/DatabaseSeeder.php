9de9ae819097acf0d25c4b74d0638eab661cbc6a
Tim Habersack <tim.habersack@gmail.com>
2015-03-06 01:06:05 +0000

    have the db seeding correctly

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,30 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->call('UserTableSeeder');
+		$this->call('UserCategoriesTableSeeder');
 	}
+
+}
+
+class UserTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('users')->delete();
+
+        User::create(['email' => 'foo@bar.com','username'=>'user1','rank'=>10,'currency'=>0,'password'=>Hash::make('pass')]);
+    }
+
+}
+
+class UserCategoriesTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('user_categories')->delete();
+
+        User_category::create(['uid'=>1,'category_name'=>'default fill','balance'=>0.00,'top_limit'=>0.00,'saved'=>0.00,'class'=>255,'rank'=>1000]);
+    }
 
 }
